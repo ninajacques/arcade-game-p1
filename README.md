02c814590ccb815b3e8acf09034e42f9817b5a63
ninajacques <elismjacques@gmail.com>
2019-03-10 21:31:34 -0300

    Revisao 1 Projeto1

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ frontend-nanodegree-arcade-game
 Os alunos devem usar esta [rubrica](https://review.udacity.com/#!/rubrics/436/view) para fazer uma autoavaliação de seu projeto. Certifique-se de que as funções que escreveu sejam **orientadas a objetos** - tanto funções de classe (como Player e Enemy) como funções de protótipo de classe, como Enemy.prototype.checkCollisions, e que a palavra-chave 'this' seja usada de maneira correta em suas funções de classe e de protótipo de classe para se referir ao objeto que faz a chamada da função. Certifique-se, também, de que o arquivo **readme.md** esteja atualizado com suas instruções, tanto sobre rodar quanto sobre como jogar o fliperama clássico.
 
 Para ver instruções detalhadas sobre como começar, confira este [guia](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+
+
+INSTRUÇÕES DO JOGO
+
+Você deve rodar o arquivo index.html em seu navegador de preferência. Ao abrir, será iniciado o jogo, com o cenário e o jogador, começando a aparecer os inimigos depois.
+Para movimentar o jogador, use as teclas de direção do teclado (left, right, up e down).
+Ao colidir com um inimigo, o jogo enviará um alerta de que o jogador perdeu o jogo e deve-se clicar em OK ou apertar enter para recomeçar.
+Ao chegar no rio, que é o objetivo do jogo, o navegador enviará um alerta de que o jogador ganhou e deve-se clicar em OK ou apertar enter para jogar novamente.
--- js/app.js
+++ js/app.js
@@ -1,10 +1,22 @@
-// Inimigos que nosso jogador deve evitar
-class Enemy {
-    constructor(y) {
-        this.x = -50;
+// Super Classe com dados comuns em Enemy e Player
+class GameElements {
+    constructor(x,y) {
+        this.x = x;
         this.y = y;
+    }
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
+
+// Classe Enemy usada para criar os inimigos do jogo e suas funções
+class Enemy extends GameElements {
+    //Método construtor com as informações basicas do objeto
+    constructor(x,y) {
+        super(x,y)
         this.sprite = 'images/enemy-bug.png';   
     }
+    //Verifica se houve a colisão entre jogador e inimigo
     collision() {
         return ((this.x + 80 > player.x
             && this.x + 80 < player.x + 101
@@ -12,13 +24,11 @@ class Enemy {
             && this.x < player.x + 80))
             && this.y === player.y;
     }
-    render() {
-        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    }
-    update(dt) {
+    //Atualiza o jogo e avisa caso ocorra a colisão
+    update(dt) { 
         if (this.collision()) {
             alert('Você foi pego! Aperte enter para reiniciar o jogo!');
-            reset();
+            player.reset();
         } else {
             const speed = Math.floor(Math.random() * (300 - 100)) + 100;
             return this.x += (speed*dt);
@@ -26,33 +36,32 @@ class Enemy {
     }
 };
 
-// Agora, escreva sua própria classe de jogador
-// Esta classe exige um método update(), 
-// um render() e um handleInput().
-class Player {
-    constructor() {
-        this.x = 200;
-        this.y = 440;
+// Classe Player usada para criar o jogador e suas funções
+class Player extends GameElements { //
+    //Método construtor com as informações basicas do objeto
+    constructor(x,y) {
+        super(x,y);
         this.sprite = 'images/char-boy.png';
     }
+    //Reinicia o jogo em caso de vitória ou derrota
     reset() {
         this.x = 200;
         this.y = 440;
+        allEnemies = [];
     }
+    //Atualiza o jogo e avisa caso o jogador consiga chegar no rio e vencer
     update(dt) {
         if (this.y === -40) {
             alert('Você venceu! Aperte enter para jogar novamente!');
-            reset();
+            this.reset();
         } else{
             const speed = Math.floor(Math.random() * (100 - 50)) + 50;
             return speed;
         }
     }
-    render() {
-        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    }
 };
 
+//Identifica quando há movimentação no jogo e limita o espaço em que o jogador pode andar
 Player.prototype.handleInput = function(key) {
   switch(key) {
     case 'left':
@@ -78,26 +87,19 @@ Player.prototype.handleInput = function(key) {
   }
 };
 
-const reset = function() {
-    player.reset();
-    allEnemies = [];
-};
-
 // Represente seus objetos como instâncias.
 // Coloque todos os objetos inimgos numa array allEnemies
-// Coloque o objeto do jogador numa variável chamada jogador.
-
+// Coloque o objeto do jogador numa variável chamada player.
+let player = new Player(200,440);
 let allEnemies = [];
 
 let callEnemies = function() {
     setInterval(function() {
         const num = [40,120,200];
-        let newEnemy = allEnemies.push(new Enemy(num[Math.floor(Math.random()*3)]));
+        let newEnemy = allEnemies.push(new Enemy(-50,num[Math.floor(Math.random()*3)]));
     },800);    
 };
 callEnemies();
-
-let player = new Player();
 
 // Isto reconhece cliques em teclas e envia as chaves para seu
 // jogador. método handleInput(). Não é preciso mudar nada.
